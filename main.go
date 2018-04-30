90ec5284078309ca3feabd73378791a4cc8d1ae1
Jenda Mudron <jenmud@gmail.com>
2018-04-30 11:08:24 +1000

    Add files via upload

--- main.go
+++ main.go
@@ -3,25 +3,28 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(w, "Hello World")
-	t, err := template.ParseFiles(
-		"views/index.html",
-		"views/header.html",
-		"views/footer.html",
+	t := template.Must(
+		template.ParseFiles(
+			"views/index.html",
+			"views/header.html",
+			"views/footer.html"),
 	)
-	if err != nil {
-		fmt.Fprintf(w, err.Error())
+
+	if err := t.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	t.ExecuteTemplate(w, "index", nil)
+
 }
 
 func main() {
 	fmt.Println("Listening on port 8080")
 	http.HandleFunc("/", index)
-
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
